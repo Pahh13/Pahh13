ce228bb9c605a06c3d7ea5f75cdc72d20bf70ab2
Pahh13 <patresedossantos@gmail.com>
2017-10-03 02:32:46 -0300

    Alteracoes bootbox

---
+++ assets/js/palestrantes.js
@@ -0,0 +1,69 @@
+function teste(val){
+
+	if(val == 1){
+
+    var dialog = bootbox.dialog({
+    title: 'Gustavo Belizario (Head of Sales - Smart Talk)',
+    message: "<p>Com mais de 8 anos de experiencia na area comercial e hard skill em inside sales possui passagens por multinacionais do mercado digital como Groupon e Beblue, hoje esta a frente na operação da Smart Talk com objetivo de expandir o segmento de comunicação corporativa de impacto para todo o país.</p>",
+    buttons: {
+        ok: {
+            label: "Voltar",
+            className: 'btn-info',
+            callback: function(){
+                
+            }
+        }
+    }
+    });
+
+    }else if(val == 2){
+
+    var dialog = bootbox.dialog({
+    title: 'Victor de Melo (Mídia Performance/Programática e Supervisor de Canais da Cadreon)',
+    message: "<p>Ampla vivência e experiência no mercado digital, o que para muitas empresas é um tema novo e tabu AINDA. Trabalha com mídia programática para grandes empresas como Unilever, Coca-Cola, entre outras. <br>Já foi consultor de mídias da Publicis. E vai falar um pouco sobre novos meios de compras e perfil do comprador de BitCoin</p>",
+    buttons: {
+        ok: {
+            label: "Voltar",
+            className: 'btn-info',
+            callback: function(){
+                
+            }
+        }
+    }
+    });
+
+    }else if(val == 3){
+
+    var dialog = bootbox.dialog({
+    title: 'Ricardo corrêa (CEO - Ramper)',
+    message: "<p>Liderou departamentos de marketing de empresas como Strattus e Aurum Software, onde empreendeu programas de geração de leads e foi um dos precursores do movimento de inbound marketing no Brasil. <br>Em 2012, fundou a Siteina e em 2016, fundou a Ramper, empresa que desenvolve software de automação de prospecção de vendas B2B que possibilita gerar mais leads via outbound e manter a equipe comercial focada nas atividades de alto valor.</p>",
+    buttons: {
+        ok: {
+            label: "Voltar",
+            className: 'btn-info',
+            callback: function(){
+                
+            }
+        }
+    }
+    });
+
+	}else if(val == 4){
+
+    var dialog = bootbox.dialog({
+    title: 'FOXBIT',
+    message: "<p> Teremos a honra de ter um convidado surpresa para todos os participantes. Podemos adiantar que é uma das maiores Startups, a FoxBit. E o convidado surpresa terá outras surpresas para todos nós. Não percam !</p>",
+    buttons: {
+        ok: {
+            label: "Voltar",
+            className: 'btn-info',
+            callback: function(){
+                
+            }
+        }
+    }
+    });
+
+	}
+
+}
